35240bbe6edbb630e2f6bc01a9ca97132457ef27
khirthikumar <khirthikumar1215@gmail.com>
2023-07-18 23:08:16 +0530

    first commit

---
+++ src/main/java/com/usecase/calculator/calculatorUsecase/CalculatorUsecaseApplication.java
@@ -0,0 +1,22 @@
+package com.usecase.calculator.calculatorUsecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class CalculatorUsecaseApplication {
+
+	public static void main(String[] args) {
+
+		Calculator myCalculator = new Calculator();
+		int num1 = 20;
+		int num2 = 10;
+		System.out.println("Sum = " + myCalculator.add(num1, num2));
+		System.out.println("Difference = " + myCalculator.subtract(num1, num2));
+		System.out.println("Product = " + myCalculator.multiply(num1, num2));
+		System.out.println("Quotient = " + myCalculator.divide(num1, num2));
+	}
+
+}
---
+++ src/test/java/com/usecase/calculator/calculatorUsecase/CalculatorUsecaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usecase.calculator.calculatorUsecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorUsecaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
