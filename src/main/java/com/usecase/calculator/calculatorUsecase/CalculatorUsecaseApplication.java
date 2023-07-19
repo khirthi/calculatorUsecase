313df6de43f22af1fcc80c2679d6c5cd3090544f
khirthikumar <khirthikumar1215@gmail.com>
2023-07-19 11:33:23 +0530

    added sample testcases

--- src/main/java/com/usecase/calculator/calculatorUsecase/CalculatorUsecaseApplication.java
+++ src/main/java/com/usecase/calculator/calculatorUsecase/CalculatorUsecaseApplication.java
@@ -17,6 +17,9 @@ public class CalculatorUsecaseApplication {
 		System.out.println("Difference = " + myCalculator.subtract(num1, num2));
 		System.out.println("Product = " + myCalculator.multiply(num1, num2));
 		System.out.println("Quotient = " + myCalculator.divide(num1, num2));
+
+		num2 = 0;
+		System.out.println("Quotient = " + myCalculator.divide(num1, num2)); //throws ArithmeticExceptionError
 	}
 
 }
